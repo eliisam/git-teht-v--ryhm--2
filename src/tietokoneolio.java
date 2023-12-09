a903f71d13b828fbaf464bcda28096fcca0796f0
Joakim23001 <joakim.luoto@student.hamk.fi>
2023-12-09 16:04:51 +0200

    Lisäyksiä koodiin
    Koneelle määritetty muistia, ominaisuudet ja konstruktori lisätty,  getter muistille llisätty, sekä tulostaTiedot metodi.

--- src/tietokoneolio.java
+++ src/tietokoneolio.java
@@ -4,10 +4,10 @@ public class tietokoneolio {
 	public static void main(String[] args) {
 		
 		// Muodostetaan tietokone-luokasta olio
-		Tietokone tietokone1 = new Tietokone();
+		Tietokone tietokone1 = new Tietokone(8); //muisti määritetty heti luonnin yhteydessä
 		tietokone1.kaynnisty();
 		tietokone1.sammu();
-		
+		tietokone1.tulostaTiedot(); 
 	}
 
 } // Main -luokan lopetus
@@ -16,13 +16,36 @@ public class tietokoneolio {
 // Tietokone-luokka
 class Tietokone {
 	
+	// ominaisuudet
+	private boolean kaynnissa;
+	private int muisti;
+	
+	// konstruktori
+	public Tietokone(int alkuMuisti) {
+		this.kaynnissa = false;
+		this.muisti = alkuMuisti;
+	}
+	
 	// toiminnallisuudet
 	public void kaynnisty() {
+		this.kaynnissa = true;
 		System.out.println("Tietokone on nyt käynnistetty");
 	}
 	
 	public void sammu() {
+		this.kaynnissa = false;
 		System.out.println("Tietokone sammutetaan");
 	}
+	
+	// getter muistille
+	public int getMuisti() {
+		return muisti;
+	}
+	 public void tulostaTiedot() {
+	        System.out.println("Tietokoneen tiedot:");
+	        System.out.println("Käynnissä: " + kaynnissa);
+	        System.out.println("Muisti: " + muisti + " gigatavua");
+	       
+	 }
 
 }
